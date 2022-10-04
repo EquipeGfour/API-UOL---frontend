c8d1df8e8d041c71f5ab5bc8d49ab0005785b09c
Rafael Waltrick <rafael.waltrick@fatec.sp.gov.br>
2022-10-04 09:58:17 -0300

    Criado Modal da Categoria.

--- front_end-uol/src/components/cadastroProduto/cadastroProduto.tsx
+++ front_end-uol/src/components/cadastroProduto/cadastroProduto.tsx
@@ -18,9 +18,28 @@ import { InputText } from 'primereact/inputtext';
 
 const CadastroProduto: React.FC = (props) => {
 
+    let emptyProduct = {
+        id: null,
+        name: '',
+        image: null,
+        description: '',
+        category: null,
+        price: 0,
+        quantity: 0,
+        rating: 0,
+        inventoryStatus: 'INSTOCK'
+    };
+
     const [produtos, setProdutos] = useState([]);
     const [produtosSelecionados, setProdutosSelecionados] = useState([]);
-    const [globalFilter, setGlobalFilter] = useState(null)
+    const [products, setProducts] = useState(null);
+    const [productDialog, setProductDialog] = useState(false);
+    const [deleteProductDialog, setDeleteProductDialog] = useState(false);
+    const [deleteProductsDialog, setDeleteProductsDialog] = useState(false);
+    const [product, setProduct] = useState(emptyProduct);
+    const [selectedProducts, setSelectedProducts] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [globalFilter, setGlobalFilter] = useState(null);
 
 
 
@@ -32,18 +51,123 @@ const CadastroProduto: React.FC = (props) => {
     const viewimage = (produto) => {
         return <img src={`images/product/${produto.image}`} />
     }
-    React.useEffect(() => {
-        pegaDados()
+    
 
-    }, []);
+    
+    const openNew = () => {
+        setProduct(emptyProduct);
+        setSubmitted(false);
+        setProductDialog(true);
+    }
+
+    const hideDialog = () => {
+        setSubmitted(false);
+        setProductDialog(false);
+    }
+
+    const hideDeleteProductDialog = () => {
+        setDeleteProductDialog(false);
+    }
+
+    const hideDeleteProductsDialog = () => {
+        setDeleteProductsDialog(false);
+    }
+
+    const saveProduct = () => {
+        setSubmitted(true);
+
+        if (product.name.trim()) {
+            let _products = [...products];
+            let _product = {...product};
+            
+
+            setProducts(_products);
+            setProductDialog(false);
+            setProduct(emptyProduct);
+        }
+    }
+
+    const editProduct = (product) => {
+        setProduct({...product});
+        setProductDialog(true);
+    }
+
+    const confirmDeleteProduct = (product) => {
+        setProduct(product);
+        setDeleteProductDialog(true);
+    }
+
+    const deleteProduct = () => {
+        let _products = products.filter(val => val.id !== product.id);
+        setProducts(_products);
+        setDeleteProductDialog(false);
+        setProduct(emptyProduct);
+    }
+    const onInputChange = (e, name) => {
+        const val = (e.target && e.target.value) || '';
+        let _product = {...product};
+        _product[`${name}`] = val;
+
+        setProduct(_product);
+    }
+
+    const onInputNumberChange = (e, name) => {
+        const val = e.value || 0;
+        let _product = {...product};
+        _product[`${name}`] = val;
+
+        setProduct(_product);
+    }
+    
+
+    const ratingBodyTemplate = (rowData) => {
+        return <Rating value={rowData.rating} readOnly cancel={false} />;
+    }
+
+    const statusBodyTemplate = (rowData) => {
+        return <span className={`product-badge status-${rowData.inventoryStatus.toLowerCase()}`}>{rowData.inventoryStatus}</span>;
+    }
+
+    const findIndexById = (id) => {
+        let index = -1;
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
     const leftToolbarTemplate = () => {
         return (
             <React.Fragment>
-                <Button label="Novo" icon="pi pi-plus"  className="p-button-success mr-1" />
+                <Button label="Novo" icon="pi pi-plus"  className="p-button-success mr-1"  />
+                <Button label="Categoria" icon="pi pi-plus"  className="p-button-success mr-1" onClick={openNew} />
                 <Button label="Delete" icon="pi pi-trash" className="p-button-danger" />
             </React.Fragment>
         )
     }
+
+    class ProductService {
+
+        getProductsSmall() {
+            return fetch('data/products-small.json').then(res => res.json()).then(d => d.data);
+        }
+    
+        getProducts() {
+            return fetch('data/products.json').then(res => res.json()).then(d => d.data);
+        }
+    
+        getProductsWithOrdersSmall() {
+            return fetch('data/products-orders-small.json').then(res => res.json()).then(d => d.data);
+        }
+    }
+    React.useEffect(() => {
+        pegaDados()
+
+    }, []);
 
     const header = () => {
         return(
@@ -56,6 +180,11 @@ const CadastroProduto: React.FC = (props) => {
             </div>
             )
     }
+    const onCategoryChange = (e) => {
+        let _product = {...product};
+        _product['category'] = e.value;
+        setProduct(_product);
+    }
     const setfilter = (e) =>{
         if(e === ''){
             setGlobalFilter(null)
@@ -64,7 +193,20 @@ const CadastroProduto: React.FC = (props) => {
             setGlobalFilter(e)
         }
     }
+    const productDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" icon="pi pi-times" className="p-button-text" onClick={hideDialog} />
+            <Button label="Save" icon="pi pi-check" className="p-button-text" onClick={saveProduct} />
+        </React.Fragment>
+    );
+    
 
+    const deleteProductsDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" className="p-button-text" onClick={hideDeleteProductsDialog} />
+            <Button label="Yes" icon="pi pi-check" className="p-button-text"  />
+        </React.Fragment>
+    );
 
     return (
         <>
@@ -82,7 +224,18 @@ const CadastroProduto: React.FC = (props) => {
                         <Column field='category' header='Categoria'></Column>
                     </DataTable>
                 </div>
-            </div>   
+            </div> 
+
+            <Dialog visible={productDialog} style={{ width: '750px' }} header="Product Details" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+                {product.image && <img src={`images/product/${product.image}`} />}
+                <div className="field">
+                    <label htmlFor="name">Name</label>
+                    <InputText id="name" value={product.name} onChange={(e) => onInputChange(e, 'name')} required autoFocus className={classNames({ 'p-invalid': submitted && !product.name })} />
+                    {submitted && !product.name && <small className="p-error">Name is required.</small>}
+                </div>
+            </Dialog>
+
+            
         </>
     );
 }
