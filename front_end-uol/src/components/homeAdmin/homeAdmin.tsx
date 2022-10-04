37944271c9be9a1fdad129627f5b4ff64f713a0b
Raniel <raniel.paula@fatec.sp.gov.br>
2022-10-04 16:11:40 -0300

    Ajuste de layout do componente

--- front_end-uol/src/components/homeAdmin/homeAdmin.tsx
+++ front_end-uol/src/components/homeAdmin/homeAdmin.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
 import './homeAdmin.css'
 import { Link } from "react-router-dom";
 
 
 
 const HomeAdmin: React.FC = (props) => {
-
-
     return (    
         <div>
            <div className="containerCadastro">
@@ -16,7 +12,6 @@ const HomeAdmin: React.FC = (props) => {
             <div className="collection home">
                 <Link to={'/cadastro-produto'} className="collection-item pointer">Produtos e Categorias</Link>
                 <Link to={'/cadastro-pacote'} className="collection-item pointer">Criação e Gerenciamento de Pacotes</Link>
-                <Link to={'/cadastro-sugestoes'} className="collection-item pointer">Gerenciamento de Sugeridos</Link>
             </div>
         </div>
         </div>
