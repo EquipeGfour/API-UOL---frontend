b6a4e7a567fa3ba79208d1047faffc5d1815b658
Nicolas Lima <nichollaslimma734@gmail.com>
2022-10-08 23:16:20 -0300

    ajustes no design e corrção de bugs

--- front_end-uol/src/components/cadastroProduto/cadastroProduto.tsx
+++ front_end-uol/src/components/cadastroProduto/cadastroProduto.tsx
@@ -35,18 +35,18 @@ const CadastroProduto: React.FC = () => {
   const [globalFilter, setGlobalFilter] = useState(null);
   const [categoria, setCategoria] = useState(null);
   const [categorias, setCategorias] = useState([]);
-  const [categoriaSelecionada, setCategoriaSelecionada] = useState(null);
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState<any[]>([])
   const [produtosSugestao, setProdutosSugestao] = useState(null);
   const [nome, setNome] = useState('');
   const [descricao, setDescricao] = useState('');
   const [preco,setPreco] = useState('');
-  const [sugestao, setSugestao] = useState('');
+  const [sugestao, setSugestao] = useState<any[]>([]);
   const toast = useRef(null);
 
 
-
-
-  // AXIOS "GET"
+  /**
+   * Axios Get
+   */
   const buscarProdutos = () => {
     axios
       .get("http://localhost:8080/produto/buscar")
@@ -58,6 +58,9 @@ const CadastroProduto: React.FC = () => {
       });
   };
 
+  /**
+   * Axios Get
+   */
   const buscarCategoria = () => {
     axios.get(`http://localhost:8080/categoria/buscar`).then((res) => {
       setCategorias(res.data)
@@ -66,15 +69,12 @@ const CadastroProduto: React.FC = () => {
     })
   }
 
+  /**
+   * Axios Get
+   */
   const buscarCategoriaId = (e) =>{
-
     if(e.value.length){
-      let idsFormatado = ''
-      e.value.forEach((arrayItem) =>{
-        idsFormatado = idsFormatado + arrayItem.id + ',';
-      });
-      let ids = idsFormatado.slice(0, -1)
-
+      let ids = e.value.map((arrayItem)=>arrayItem.id).join(',')
       setCategoriaSelecionada(e.value)
       axios.get(`http://localhost:8081/compra/selecionar-sugestoes/${ids}`).then((res) => {
         setProdutosSugestao(res.data)
@@ -82,13 +82,14 @@ const CadastroProduto: React.FC = () => {
         console.error("Erro", erro.response);
       })
     }else{
-      setCategoriaSelecionada(null)
-      setProdutosSugestao(null)
+      setCategoriaSelecionada([])
+      setProdutosSugestao([])
     }
   }
 
-
-  // AXIOS POST
+  /**
+   * Axios Post
+   */
   const cadastrarCategoria = () => {
     axios.post("http://localhost:8080/categoria/cadastrar", { nome: categoria }).then((res) => {
       setCategoria("")
@@ -99,6 +100,9 @@ const CadastroProduto: React.FC = () => {
     })
   }
 
+  /**
+   * Axios Post
+   */
   const cadastrarProduto = () => {
     let dados = {
       nome:nome,
@@ -110,9 +114,9 @@ const CadastroProduto: React.FC = () => {
     axios.post("http://localhost:8080/produto/cadastrar",dados).then((res)=>{
       setNome('');
       setDescricao('');
-      setCategoriaSelecionada('');
+      setCategoriaSelecionada([]);
       setPreco('');
-      setSugestao('');
+      setSugestao([]);
       hide1Dialog();
       toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Produto Cadastrada', life: 3000 });
       buscarProdutos();
@@ -122,7 +126,9 @@ const CadastroProduto: React.FC = () => {
   }
 
 
-  // AXIOS DELETE
+  /**
+   * Axios Delete
+   */
   const deletaProduto = (e) => {
     axios.delete(`http://localhost:8080/produto/excluir/${e.id}`).then(res => {
       const Novalista = produtos.filter((p) => p.id !== e.id)
@@ -138,9 +144,16 @@ const CadastroProduto: React.FC = () => {
     buscarCategoria();
   }, []);
 
-  // const viewimage = (produto) => {
-  //   return <img src={"images/product/uol.jpg"} />;
-  // };
+  useEffect(()=>{
+    if(!product1Dialog){
+      setCategoriaSelecionada([])
+      setProdutosSugestao([])
+      setSugestao([])
+      setNome('');
+      setDescricao('');
+      setPreco('');
+    }
+  }, [product1Dialog]);
 
   const openNew = () => {
     setProduct(emptyProduct);
@@ -168,15 +181,15 @@ const CadastroProduto: React.FC = () => {
     return (
       <React.Fragment>
         <Button
-          label="Novo"
+          label="Produto"
           icon="pi pi-plus"
-          className="p-button-success mr-1 botaoTamanho"
+          className="p-button-success mr-1 botaoTamanho mt0"
           onClick={open1New}
         />
         <Button
           label="Categoria"
           icon="pi pi-plus"
-          className="p-button-success mr-1 botaoTamanho"
+          className="p-button-success mr-1 botaoTamanho mt0"
           onClick={openNew}
         />
         {/* <Button
@@ -198,7 +211,7 @@ const CadastroProduto: React.FC = () => {
         /> */}
         <Button
           icon="pi pi-trash "
-          className="p-button-rounded p-button-warning botaoTamanho excluir"
+          className="p-button-rounded p-button-warning botaoTamanho excluir mt0 bt-delete-hover"
           onClick={() => deletaProduto(rowData)}
         />
       </React.Fragment>
@@ -207,11 +220,11 @@ const CadastroProduto: React.FC = () => {
 
   const categoriaTemplate = (rowData) => {
     return (
-      <ul>
+      <div>
         {rowData.categorias?.map((c) => (
           <li className="formatarFont" key={c.id}>{c.nome}</li>
         ))}
-      </ul>
+      </div>
     );
   };
 
@@ -251,7 +264,7 @@ const CadastroProduto: React.FC = () => {
       <Button
         label="Cancelar"
         icon="pi pi-times"
-        className="p-button-text botaoTamanho"
+        className="p-button-text botaoTamanho bt-dialog"
         onClick={hideDialog}
       />
       <Button
@@ -260,7 +273,7 @@ const CadastroProduto: React.FC = () => {
           cadastrarCategoria()
         }}
         icon="pi pi-check"
-        className="p-button-text botaoTamanho"
+        className="p-button-text botaoTamanho bt-dialog"
       />
     </React.Fragment>
   );
@@ -270,13 +283,13 @@ const CadastroProduto: React.FC = () => {
       <Button
         label="Cancelar"
         icon="pi pi-times"
-        className="p-button-text botaoTamanho "
+        className="p-button-text botaoTamanho bt-dialog"
         onClick={hide1Dialog}
       />
       <Button
         label="Salvar"
         icon="pi pi-check"
-        className="p-button-text botaoTamanho"
+        className="p-button-text botaoTamanho bt-dialog"
         onClick={cadastrarProduto}
       />
     </React.Fragment>
@@ -287,8 +300,9 @@ const CadastroProduto: React.FC = () => {
       <div className="datatable-crud-demo">
         <Toast ref={toast} />
         <div className="card">
-          <Toolbar left={leftToolbarTemplate}></Toolbar>
+          <Toolbar className="br0" left={leftToolbarTemplate}></Toolbar>
           <DataTable
+          className="dt-select-field dt-filtro-valor-cor"
             value={produtos}
             selection={produtosSelecionados}
             onSelectionChange={(e) => setProdutosSelecionados(e.value)}
@@ -324,19 +338,18 @@ const CadastroProduto: React.FC = () => {
         style={{ width: "750px" }}
         header="Criar Categoria"
         modal
-        className="p-fluid"
+        className="p-fluid pb0-input-dialog-categoria"
         footer={botoesCategoria}
         onHide={hideDialog}
       >
         {product.image && <img src={`images/product/${product.image}`} />}
-        <div className="field">
+        <div className="field mbt0">
           <label htmlFor="name">Categoria</label>
           <InputText
             id="name"
             value={categoria}
             onChange={(e) => setCategoria(e.target.value)}
             required
-            autoFocus
             className={classNames({ "p-invalid": submitted && !product.name })}
           />
           {submitted && !product.name && (
@@ -362,7 +375,6 @@ const CadastroProduto: React.FC = () => {
             value={nome}
             onChange={(e) => setNome(e.target.value)}
             required
-            autoFocus
             className={classNames({ "p-invalid": sub1mitted && !product.name })}
           />
           {sub1mitted && !product.name && (
@@ -388,7 +400,6 @@ const CadastroProduto: React.FC = () => {
             value={preco}
             onChange={(e) => setPreco(e.target.value)}
             required
-            autoFocus
             className={classNames({
               "p-invalid": sub1mitted && !product.price,
             })}
@@ -400,6 +411,7 @@ const CadastroProduto: React.FC = () => {
         <div className="field">
           <label htmlFor="Vincular">Vincular Categoria</label>
           <MultiSelect
+          className="mult-chips-bgcolor-color"
             value={categoriaSelecionada}
             options={categorias}
             onChange={(e) => buscarCategoriaId(e)}
@@ -411,15 +423,18 @@ const CadastroProduto: React.FC = () => {
         <div className="field">
           <label htmlFor="Sugestões">Sugestões de Produtos</label>
           <MultiSelect
+            className="mult-chips-bgcolor-color"
             value={sugestao}
             options={produtosSugestao}
             onChange={(e) => setSugestao(e.value)}
             optionLabel="nome"
             placeholder="Selecione a Categoria"
             display="chip"
+            disabled={!categoriaSelecionada.length}
           />
         </div>
       </Dialog>
+      <br/>
     </>
   );
 };
