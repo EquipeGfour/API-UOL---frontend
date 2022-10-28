5eaad531f33d82241b53b4f140b204028003dfc7
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-10-28 19:34:54 -0300

    Criado Interface de Cadastro de Promoçao

--- front_end-uol/src/components/cadastroPromocao/cadastroPromocao.tsx
+++ front_end-uol/src/components/cadastroPromocao/cadastroPromocao.tsx
@@ -1,11 +1,51 @@
-import React from "react";
-import './cadastroPromocao'
+import './cadastroPromocao.css'
+import { InputText } from 'primereact/inputtext';
+import React, { useState } from 'react';
+import { Chips } from 'primereact/chips';
+import { MultiSelect } from 'primereact/multiselect';
+import { Button } from 'primereact/button';
 
 const CadastroPromocaoFinal: React.FC = (props) => {
-    return (    
-        <div>
+    const [promocao, setPromocao] = useState('');
+    const [values1, setValues1] = useState<any>([]);
+    const [selectedCities3, setSelectedCities3] = useState(null);
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+    return (
+        <>
+            <div className="borda-inicial-1">
+                <div className='seletores'>
+                    <div className='espaçamento'>
+                        <label className='formato-label' htmlFor="inputtext">Promoção</label>
+                        <br />
+                        <InputText className='borda' value={promocao} onChange={(e) => setPromocao(e.target.value)} />
+                    </div>
+                    <div className="espaçamento">
+                        <span>
+                            <label htmlFor="inputtext">Selecionar Categoria</label>
+                            <br />
+                            <MultiSelect className=' ' max={5} value={selectedCities3} options={cities}  onChange={(e) => setSelectedCities3(e.value)} optionLabel="name" placeholder="Selecionar Categoria" display="chip" />
+                        </span>
+                    </div>
+                </div>
 
-        </div>
+                <div className='linha-chip'>
+                    <div className='espaçamento'>
+                        <label className='formato-label-pacotes' htmlFor="inputtext">Pacotes</label>
+                        <br />
+                        <Chips className='chip-Pacotes' value={values1} onChange={(e) => setValues1(e.value)} />
+                    </div>
+                </div>
+                <Button  label="Criar" className="p-button-success botao-criar-promocao" />
+            </div>
+            
+        </>
+
     )
 }
 
--- front_end-uol/src/components/homeAdmin/homeAdmin.tsx
+++ front_end-uol/src/components/homeAdmin/homeAdmin.tsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 const HomeAdmin: React.FC = (props) => {
     return (    
         <div>
-           <div className="containerCadastro">
+        <div className="containerCadastro">
             <h5>Bem Vindo Administrador</h5>
             <div className="collection home">
                 <Link to={'/cadastro-produto-final'} className="collection-item pointer">Produtos e Categorias</Link>
                 <Link to={'/cadastro-pacote'} className="collection-item pointer">Criação e Gerenciamento de Pacotes</Link>
                 <Link to={'/cadastro-ofertas'} className="collection-item pointer">Criação e Gerenciamento de Ofertas</Link>
-                <Link to={'/cadastro-Promocao'} className="collection-item pointer">Criação e Gerenciamento de Promoção</Link>
+                <Link to={'/cadastro-promocao-final'} className="collection-item pointer">Criação e Gerenciamento de Promoção</Link>
             </div>
         </div>
         </div>
